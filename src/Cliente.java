6de7eeef92fdf7b55fe41aefe68cb2d1bf633a81
Matheus-Soares-Duarte <matheussoares_09@hotmail.com>
2019-09-26 15:19:18 -0300

    Revert "classe Cliente desnecessaria, suas funções foram divididas entre Menu e Jogador (que agora representa o Cliente)"
    This reverts commit 7113b98990383957b8bd902893f54db4c6dd0058.

---
+++ src/Cliente.java
@@ -0,0 +1,31 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Cliente {
+//    public static void main (String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Digite um nome: ");
+//        String nome = sc.nextLine();
+//        Jogador p = new Jogador("1.1.1.1",nome);
+//        menuInicial m = new menuInicial();
+//        m.RunMenuInicial(p);
+//
+//        sc.close();
+//    }
+//
+//    public Cliente (Servidor servidor){
+//        try {
+//            Scanner sc = new Scanner(System.in);
+//
+//            System.out.print("Digite um nome: ");
+//            String nome = sc.nextLine();
+//            Jogador jogador = new Jogador(InetAddress.getLocalHost().getHostAddress(), nome);
+//
+//            Menu m = new Menu();
+//            m.menuInicial(servidor, jogador);
+//        } catch (UnknownHostException e) {
+//            e.printStackTrace();
+//        }
+//    }
+}
